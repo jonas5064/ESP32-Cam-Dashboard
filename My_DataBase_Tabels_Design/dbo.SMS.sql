248da8c2e452bd9827eaf7c38a0fac7578821c83
Αλέξανδρος Πλατάνιος <alexandrosplatanios15@gmail.com>
2021-01-02 23:09:34 +0200

    Twilio SMS Save Data 50%

---
+++ My_DataBase_Tabels_Design/dbo.SMS.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SMS]
+(
+	[Id]    INT           IDENTITY (1, 1) NOT NULL,
+    [AccountSID] NCHAR (255)   NOT NULL,
+    [AccountTOKEN]  NCHAR(255) NOT NULL,
+    [Phone] NCHAR(55) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+)
